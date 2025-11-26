78b7db959e9575fe794e49dc9100bc2990820136
rohanjoseph <rohan.joseph89@gmail.com>
2025-11-26 08:50:03 +0530

    Adding the test file

--- dataWare/tests.py
+++ dataWare/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class DataWareTests(TestCase):
+    def test_data_view(self):
+        response = self.client.get('/data')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
